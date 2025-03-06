baf404f64cbd49002e76cb486e99ac019d94000c
mina <minanasserwanaszero@gmail.com>
2025-03-06 23:43:06 +0200

    first commit sign in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gg2/cubit/todo_cubit.dart';
-import 'package:gg2/todo_list.dart';
-import 'add_todo_page.dart';
+import 'package:gg2/pallete.dart';
+
+import 'login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,22 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (_)=>TodoCubit())
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (_) => const TodoList(),
-          '/add-todo': (_) => const AddTodoPage(),
-        },
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Pallete.backgroundColor,
       ),
+      home: const LoginScreen(),
     );
   }
 }
