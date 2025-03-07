add55b1b7cff68ea6eaeb207ea26fac1fe6b5ce3
mina <minanasserwanaszero@gmail.com>
2025-03-07 23:57:51 +0200

    first commit weather app with bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gg2/app_bloc_observer.dart';
-import 'package:gg2/bloc/auth_bloc.dart';
-import 'package:gg2/pallete.dart';
-
-import 'login_screen.dart';
+import 'package:weather_app/weather_screen.dart';
 
 void main() {
-  Bloc.observer = AppBlocObserver();
   runApp(const MyApp());
 }
 
@@ -16,15 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AuthBloc(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: Pallete.backgroundColor,
-        ),
-        home: const LoginScreen(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(useMaterial3: true),
+      home: const WeatherScreen(),
     );
   }
 }
